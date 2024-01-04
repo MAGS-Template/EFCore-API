182717e41693772d4105df93cef83e267cd6133a
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2024-01-04 09:44:24 +0100

    Added basic structure

---
+++ DomainModels/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels
+{
+    public class Post : Common
+    {
+        public string imageURL {  get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ DomainModels/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainModels
+{
+    public class User : Common
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public List<Post> posts { get; set; }
+        public List<User> friends { get; set; }
+    }
+}
